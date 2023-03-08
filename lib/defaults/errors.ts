4e69242904affb065830721afc020e69a88f0a39
Heitor Danilo <heitordanilo2003@gmail.com>
2023-03-07 22:00:28 -0300

    default errors configuration

---
+++ lib/defaults/errors.ts
@@ -0,0 +1,38 @@
+/**
+ * (c) 2023 Heitor Danilo
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+
+import * as Types from '../types';
+
+export const errors = {
+  default: {
+    code: 500,
+    body: {
+      status: 'error',
+      errors: ['Unexpected error. Try again later or contact support.'],
+    },
+  },
+  invalidApiKey: {
+    code: 401,
+    body: { status: 'error', errors: ['Invalid "x-api-key" header.'] },
+  },
+  invalidContentType: {
+    code: 415,
+    body: { status: 'error', errors: ['Invalid "content-type" header.'] },
+  },
+  invalidContentLength: {
+    code: 415,
+    body: { status: 'error', errors: ['Invalid "content-length" header.'] },
+  },
+  invalidRoute: {
+    code: 404,
+    body: { status: 'error', errors: ['Invalid route.'] },
+  },
+  invalidCustomHeader(errors: Array<string>): Types.Server.ServerError {
+    return {
+      code: 400,
+      body: { status: 'error', errors },
+    };
+  },
+};

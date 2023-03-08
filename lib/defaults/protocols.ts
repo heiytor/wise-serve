b6b6be24eb623e198fb30c3eab028647876e737f
Heitor Danilo <heitordanilo2003@gmail.com>
2023-03-07 22:00:38 -0300

    default protocols configuration

---
+++ lib/defaults/protocols.ts
@@ -0,0 +1,17 @@
+/**
+ * (c) 2023 Heitor Danilo
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+
+export const protocols = {
+  http: {
+    enabled: true,
+    port: 80,
+  },
+  https: {
+    enabled: false,
+    port: 443,
+    cert: '',
+    key: '',
+  },
+};

53a30828fea5603523430bb8144747cb98fd25a4
Heitor Danilo <heitordanilo2003@gmail.com>
2023-03-07 22:00:47 -0300

    default security configuration

---
+++ lib/defaults/security.ts
@@ -0,0 +1,17 @@
+/**
+ * (c) 2023 Heitor Danilo
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+
+export const security = {
+  headers: {
+    apiKey: [],
+    contentLength: -1,
+    contentType: [],
+    custom: [],
+  },
+  limits: {
+    keepAliveTimeout: 0,
+    sizeLimit: '0',
+  },
+};

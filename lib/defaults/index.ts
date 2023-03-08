129c53bd34f2f1ce23c3e370edd2728e66b89352
Heitor Danilo <heitordanilo2003@gmail.com>
2023-03-07 22:00:09 -0300

    barrel file

---
+++ lib/defaults/index.ts
@@ -0,0 +1,8 @@
+/**
+ * (c) 2023 Heitor Danilo
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+
+export * from './errors';
+export * from './protocols';
+export * from './security';

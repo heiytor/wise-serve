46bd756ecd9d75d37343dc9f010237d881d804d3
Heitor Danilo <heitordanilo2003@gmail.com>
2023-03-07 21:57:38 -0300

    barrel file

---
+++ lib/interfaces/index.ts
@@ -0,0 +1,11 @@
+/**
+ * (c) 2023 Heitor Danilo
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+
+export * from './WiseOptions';
+export * from './WisePrivateOptions';
+export * from './Request';
+export * from './Response';
+export * from './Routes';
+export * from './Next';

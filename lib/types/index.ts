8e2a8f92ba5c5bfd07257f28b2fc551720ccb268
Heitor Danilo <heitordanilo2003@gmail.com>
2023-03-07 21:59:22 -0300

    barrel file

---
+++ lib/types/index.ts
@@ -0,0 +1,7 @@
+/**
+ * (c) 2023 Heitor Danilo
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+
+export * as Route from './Route';
+export * as Server from './Server';
